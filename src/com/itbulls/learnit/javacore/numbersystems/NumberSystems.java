649299ccb0c96ccb7428af02c17966a620682c46
BONON123 <bononjunior1@gmail.com>
2024-08-14 08:11:27 +0100

    Update NumberSystems.java

--- src/com/itbulls/learnit/javacore/numbersystems/NumberSystems.java
+++ src/com/itbulls/learnit/javacore/numbersystems/NumberSystems.java
@@ -1,17 +1,17 @@
 package com.itbulls.learnit.javacore.numbersystems;
 
 public class NumberSystems {
-	
-	public static void main(String[] args) {
-		int dec = 152;            //  no prefix   --> decimal literal
-		int bin = 0b10011000;     // '0b' prefix --> binary literal
-		int oct = 0230;           // '0' prefix  --> octal literal
-		int hex = 0x98;           // '0x' prefix --> hexadecimal literal
-		
-		System.out.println(dec);
-		System.out.println(bin);
-		System.out.println(oct);
-		System.out.println(hex);
-	}
-
+    
+    public static void main(String[] args) {
+        int dec = 152; // Déclaration d'un entier décimal (sans préfixe)
+        int bin = 0b10011000; // Déclaration d'un entier binaire (préfixe '0b')
+        int oct = 0230; // Déclaration d'un entier octal (préfixe '0')
+        int hex = 0x98; // Déclaration d'un entier hexadécimal (préfixe '0x')
+        
+        System.out.println(dec); // Affiche la valeur décimale
+        System.out.println(bin); // Affiche la valeur binaire
+        System.out.println(oct); // Affiche la valeur octale
+        System.out.println(hex); // Affiche la valeur hexadécimale
+    }
+    
 }
