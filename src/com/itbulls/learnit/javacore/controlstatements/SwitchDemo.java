14fd909eeab33b536b0a0497854baac6f6c9fc86
BONON123 <bononjunior1@gmail.com>
2024-08-17 18:19:18 +0100

    Update SwitchDemo.java

--- src/com/itbulls/learnit/javacore/controlstatements/SwitchDemo.java
+++ src/com/itbulls/learnit/javacore/controlstatements/SwitchDemo.java
@@ -2,52 +2,54 @@ package com.itbulls.learnit.javacore.controlstatements;
 
 public class SwitchDemo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// expression types available: byte, short, char, int, String, Enum
-		String customerStatus = "premium_customer";
-		switch (customerStatus) {
-			case "guest":
-				System.out.println("Thank you for your order!");
-				break;
-			case "regular_customer":
-				System.out.println(
-						"Thank you for your purchase! Take discount 10% for your order "
-								+ "as a gratitude for staying with us.");
-				break;
-			case "premium_customer":
-				System.out.println("Wow! You are our PREMIUM customer! Just take all order "
-						+ "for 1 USD.");
-				break;
-			default:
-				System.out.println("Customer doesn't have status set.");
-		}
+        // expression types available: byte, short, char, int, String, Enum
+        String customerStatus = "premium_customer"; // Déclaration d'une variable de type String représentant le statut du client
 
-		System.out.println("============ No break demo");
-		int i = 1;
-		switch (i) {
-			case 1:
-				System.out.println("one");
-			case 2:
-				System.out.println("two");
-				break;
-			default:
-				System.out.println("This is default block");
-		}
+        switch (customerStatus) { // Début de l'instruction switch avec la variable customerStatus comme expression
+            case "guest": // Si le statut est "guest"
+                System.out.println("Merci pour votre commande !");
+                break; // Sortie du switch
+            case "regular_customer": // Si le statut est "regular_customer"
+                System.out.println(
+                        "Merci pour votre achat ! Bénéficiez de 10% de réduction sur votre commande "
+                                + "en remerciement de votre fidélité.");
+                break; // Sortie du switch
+            case "premium_customer": // Si le statut est "premium_customer"
+                System.out.println("Wow ! Vous êtes notre client PREMIUM ! Toute votre commande à 1 USD !");
+                break; // Sortie du switch
+            default: // Si aucun cas ne correspond
+                System.out.println("Le statut du client n'est pas défini.");
+        }
 
-		System.out.println("============ Default block demo");
+        System.out.println("============ Démonstration sans break");
+        int i = 1; // Déclaration d'une variable de type int avec la valeur 1
 
-		i = 10;
-		switch (i) {
-			case 1:
-				System.out.println("one");
-			case 2:
-				System.out.println("two");
-				break;
-			default:
-				System.out.println("This is default block");
-		}
+        switch (i) { // Début de l'instruction switch avec la variable i comme expression
+            case 1:
+                System.out.println("un");
+            case 2:
+                System.out.println("deux");
+                break; // Sortie du switch
+            default:
+                System.out.println("Ceci est le bloc par défaut");
+        }
 
-	}
+        System.out.println("============ Démonstration du bloc par défaut");
+
+        i = 10; // Modification de la valeur de i à 10
+
+        switch (i) { // Début de l'instruction switch avec la variable i comme expression
+            case 1:
+                System.out.println("un");
+            case 2:
+                System.out.println("deux");
+                break; // Sortie du switch
+            default:
+                System.out.println("Ceci est le bloc par défaut");
+        }
+
+    }
 
 }
