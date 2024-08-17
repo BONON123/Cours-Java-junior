c5d771901e4a49fd3d5bf81a12abd64d66bb3b74
BONON123 <bononjunior1@gmail.com>
2024-08-17 17:22:08 +0100

    Update EscapeSequences.java

--- src/com/itbulls/learnit/javacore/string/EscapeSequences.java
+++ src/com/itbulls/learnit/javacore/string/EscapeSequences.java
@@ -2,28 +2,26 @@ package com.itbulls.learnit.javacore.string;
 
 public class EscapeSequences {
 
-	public static void main(String[] args) {
-		System.out.println("===== Escape sequences =====");
-		System.out.println();
-		
-		System.out.println("My favourite book is \"The one thing\" by Gary Keller.");
-		System.out.println("Path to the source code: D:\\Java\\sources");
-		
-		/*
-		 	\t - tab.
-			\b - backspace (a step backward in the text or deletion of a single character).
-			\n - new line.
-			\r - carriage return. ()
-			\f - form feed.
-			\' single quote.
-			\" double quote.
-			\\ backslash.
-		 */
-		
-		System.out.println("some text line 1\nsome text line 2");
-		
-		System.out.println("Unicode characters, \u00A9 IT-Bulls.com");
+    public static void main(String[] args) {
+        System.out.println("===== Séquences d'échappement =====");
+        System.out.println();
 
-	}
-	
+        System.out.println("Mon livre préféré est \"The one thing\" par Gary Keller."); // Utilisation de \" pour inclure des guillemets dans la chaîne
+        System.out.println("Chemin vers le code source : D:\\Java\\sources"); // Utilisation de \\ pour inclure un backslash dans le chemin
+
+        /*
+            \t - Tabulation horizontale.
+            \b - Retour arrière (un pas en arrière dans le texte ou suppression d'un caractère).
+            \n - Nouvelle ligne.
+            \r - Retour chariot (moins utilisé de nos jours).
+            \f - Saut de page (peu utilisé).
+            \' - Apostrophe (simple quote).
+            \" - Guillemet double.
+            \\ - Backslash.
+         */
+
+        System.out.println("ligne de texte 1\nligne de texte 2"); // \n pour un saut de ligne
+
+        System.out.println("Caractères Unicode, \u00A9 IT-Bulls.com"); // \u00A9 pour insérer le symbole copyright ©
+    }
 }
